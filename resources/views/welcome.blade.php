3ac78a8717ec3a87cab52fa68cf35b6db4daf07c
Tiroslav Trninic <miroslav.trninic@gmail.com>
2015-04-23 16:50:51 +0200

    initial commit for login depot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,8 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">Welcome to Login Depot</div>
 			</div>
 		</div>
 	</body>
